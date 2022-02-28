ab0cd6e0c882850f094a5f20ca59e4b4edd0d850
Onur Cayci <onur.cayci@outlook.com>
2022-02-28 18:10:10 -0500

    Implement the common webpack configuration

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: "./src/index.tsx",
+	output: {
+		filename: "./[name].bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.tsx?$/,
+				use: ["ts-loader"],
+				exclude: /node_modules/
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./public/index.html",
+		}),
+	],
+	resolve: {
+		extensions: [".tsx", "ts"],
+	},
+}
